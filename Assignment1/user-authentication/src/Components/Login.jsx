9472ff3dcbc0fa6798d7f67cd2da428597cfd6cb
Vishaka Vinod <vs235403@dal.ca>
2024-06-03 20:02:51 -0300

    added main image on login page

--- Assignment1/user-authentication/src/Components/Login.jsx
+++ Assignment1/user-authentication/src/Components/Login.jsx
@@ -1,11 +1,23 @@
 import Navbar from './Navbar';
 import './styles.css';
+import loginImg from '../images/login-image.png';
 
 function Login(){
     return(
         <div>
             <Navbar/>
-            
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="p-3 img-col">
+                            <img src={loginImg} alt="Image of an event stall" id="login-img" />
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="p-3 login-form">Column 2</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
@@ -14,4 +26,5 @@ export default Login;
 
 /**
  * Image taken and modified from: https://www.canva.com/templates/EAFweV1TAJ8-company-website-business-website-in-blue-white-black-2d-illustration-style/
+ * 
  */
